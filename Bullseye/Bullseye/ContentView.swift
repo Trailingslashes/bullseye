2270c281e3a3e233b8fd3c08e93e82a8fe4acbd4
Cole Phillips <colephillips1@gmail.com>
2022-10-22 18:29:26 -0400

    updated styling for contentview

--- Bullseye/Bullseye/ContentView.swift
+++ Bullseye/Bullseye/ContentView.swift
@@ -13,15 +13,27 @@ struct ContentView: View {
         VStack {
             VStack {
                 Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                    .bold()
+                    .kerning(1)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4)
+                    .font(.title2)
             }
+        
             Text("89")
+                .font(.largeTitle)
+                .kerning(-1)
+                .fontWeight(.black)
             HStack {
                 Text("1")
+                    .bold()
                 Slider(value: .constant(50), in: 1.0...100)
                 Text("100")
+                    .bold()
             }
             Button(action: {}) {
                 Text("Hit me")
+                    
             }
     
             }
