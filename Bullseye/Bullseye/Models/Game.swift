a7a96cd049089d35805e999af35d716368037ecb
Cole Phillips <colephillips1@gmail.com>
2022-10-23 12:49:47 -0400

    added unit testing for slider value

--- Bullseye/Bullseye/Models/Game.swift
+++ Bullseye/Bullseye/Models/Game.swift
@@ -11,8 +11,17 @@ struct Game {
     var target: Int = Int.random(in: 1 ... 100)
     var score: Int = 0
     var round: Int = 1
-
+    
     func points(sliderValue: Int) -> Int {
-        return 999
+        var difference: Int
+        if sliderValue > self.target {
+            difference = sliderValue - self.target
+        } else if self.target > sliderValue {
+            difference = self.target - sliderValue
+        } else {
+            difference = 0
+        }
+        var awardedPoints: Int = 100 - difference
+        return awardedPoints
     }
 }
---
+++ Bullseye/BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,35 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Cole Phillips on 2022-10-23.
+//
+
+@testable import Bullseye
+import XCTest
+
+final class BullseyeTests: XCTestCase {
+    var game: Game!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        game = Game()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        game = nil
+    }
+
+    func testScorePositive() {
+        var guess = game.target + 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+
+    func testScoreNegative() {
+        var guess = game.target - 5
+        var score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 95)
+    }
+}
